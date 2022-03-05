261d7af28a0b2a17ef5a156e32fd2469c0521046
salwasiksik <salwamohammad8121998@gmail.com>
2022-03-05 16:51:30 +0200

    add the first button relates #12

--- script.js
+++ script.js
@@ -23,13 +23,24 @@ function addTodo(e){
 }
 
 function saveToLocalStorage(todo){
-
+    let todos
+    if (localStorage.getItem(todos)=== null){
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem(todos))
+    }
     todos.push(todo)
 
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
 function getTodoListOnLoad(){
+    let todos
+    if (localStorage.getItem(todos)=== null){
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem(todos))
+    }
 
     if(localStorage.getItem('todos')){
         todos = JSON.parse(localStorage.getItem('todos'))
